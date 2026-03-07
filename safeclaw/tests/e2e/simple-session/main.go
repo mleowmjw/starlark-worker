198e97dca3fb3068b3618152aff023ee8710bc74
Michael Leow <leowmjw@users.noreply.github.com>
2026-03-08 00:22:39 +0800

    SUmmary: Try out flow to iron out DX

---
+++ safeclaw/tests/e2e/simple-session/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+        fmt.Println("Welcome to simple session!!")
+
+// Overall workflow for the user + day? Index of usage? Is like Cursor agent?
+// Simple session will have only one task running per user?
+// Starts a session; based on the agent name + session / request?
+// Detects intent then kicks off or continues session?
+}
+
